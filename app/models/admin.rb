0d0895611b9931b96ba0d97dbcb72ae6cbc57c04
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-03-16 11:10:57 +0000

    [Add]管理者ログイン画面

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
   validates :encrypted_password, length: { minimum: 6, maximum: 15 }
 end
