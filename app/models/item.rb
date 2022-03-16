f758a9a638835f77375ecf0d25c94c267c41c4a8
EC2 Default User <ec2-user@ip-172-31-31-134.ec2.internal>
2022-03-16 08:43:17 +0000

    name edit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
-  
+
   has_many :cart_items
   has_many :order_details
-  belongs_to :jenre
+  belongs_to :genre
   validates :name, length: { maximum: 40 }
-  
+
 end
