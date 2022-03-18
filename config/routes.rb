6ac8986f14ad6a0f9ab6db103d69926bc247db7c
EC2 Default User <ec2-user@ip-172-31-31-134.ec2.internal>
2022-03-18 09:57:55 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
    end
 
    namespace :public do
-   resource :genres, only: [:index]
-   resource :addresses, only: [:index, :create, :edit, :update]
-   resource :cart_items, only: [:index, :show]
-   resource :items, only: [:index, :show]
-   resource :orders, only: [:index, :show, :new, :confirm, :create, :thanks]
+   resources :genres, only: [:index]
+   resources :addresses, only: [:index, :create, :edit, :update]
+   resources :cart_items, only: [:index, :show]
+   resources :items, only: [:index, :show]
+   resources :orders, only: [:index, :show, :new, :confirm, :create, :thanks]
    resource :customers, only: [:show, :quit, :out, :edit, :update]
    get '/quit' => 'customers#quit', as: 'quit'
    patch '/withdrawal' => 'customers#withdrawal', as: 'withdrawal'
