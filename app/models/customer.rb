bd09d67aff61a57201825396804c09ef95d10d40
EC2 Default User <ohpkiki@gmail.com>
2022-03-18 03:09:18 +0000

    initial commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,15 +3,15 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  
+
   validates :last_name, :first_name, length: { maximum: 20 }
   validates :last_name_kana, :first_name_kana, length: { maximum: 60 }
   validates :address, length: { maximum: 100 }
   validates :phone_number, length: { maximum: 13 }
-  validates :encrypted_password, length: { minimum: 6, maximum: 15 }
   
+
 end
