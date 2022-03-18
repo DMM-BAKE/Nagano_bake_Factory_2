106ae8ee251e25143170908ce6fe9436bc507f56
EC2 Default User <ohpkiki@gmail.com>
2022-03-18 07:41:15 +0000

    escape

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,16 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.where(customer_id: current_user.id)
   end
 
+  def new
+    @cart_item = CartItem.new
+
   def create
+    @cart_item = CartItem.new(params[:id])
+    @cart_item.customer_id = current_user.id
+    @cart_item.save
+    redirect_to public_cart_items_paht
   end
 
   def update
@@ -13,4 +21,5 @@ class Public::CartItemsController < ApplicationController
 
   def all_destroy
   end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,10 @@ class CartItem < ApplicationRecord
   
   belongs_to :customer
   belongs_to :item
+  has_many :order_details, through: :items
+  
+  def subtotal(count)
+    
+  end
   
 end
