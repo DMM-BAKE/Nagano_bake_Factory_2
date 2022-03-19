bb4d90bc20ab9e2be3d380bffb91f3f43302b320
yukiya1006 <96877368+yukiya1006@users.noreply.github.com>
2022-03-19 07:41:30 +0000

    escape

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     root_path(resource)
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
+  end
 end
+
