d76d17924e472deb3372db162920e925d1470a8f
yukiya1006 <96877368+yukiya1006@users.noreply.github.com>
2022-03-19 05:21:05 +0000

    escape

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
    resources :cart_items, only: [:index, :show]
    resources :items, only: [:index, :show]
-   resource :orders, only: [:index, :show, :new, :confirm, :create, :thanks]
+   resources :orders, only: [:index, :show, :new, :confirm, :create, :thanks]
    resource :customers, only: [:show, :quit, :out, :edit, :update]
    get '/quit' => 'customers#quit', as: 'quit'
    patch '/withdrawal' => 'customers#withdrawal', as: 'withdrawal'
