7773d6ead0682e4cdee094d4079900b2195547e1
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-03-19 07:36:31 +0000

    save

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,4 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
-  validates :encrypted_password, length: { minimum: 6, maximum: 15 }
 end
