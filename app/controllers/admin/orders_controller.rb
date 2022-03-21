7c62d98b45c561dfdde948af8df6f84e148f2a23
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-03-21 12:55:59 +0000

    [Add]管理者注文履歴一覧、詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,33 @@
 class Admin::OrdersController < ApplicationController
+   before_action :authenticate_admin!
+
   def index
+    @orders = Order.all.page(params[:page])
   end
 
-  def create
+  def order_customer
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @total_payment = @order.total_payment - 800
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:notice] = "注文ステータスを変更しました"
+      redirect_to admin_order_path(@order)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
